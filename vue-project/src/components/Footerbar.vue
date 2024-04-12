ad2b92daf2dc2789287f18d60b52367cdcfa1c80
DevCarlosOli <c.oliveiradev@gmail.com>
2024-04-12 12:15:53 -0300

    Deletando arquivos json da raiz, movendo para dentro do projeto, iniciando estrutura Navbar e Footer

---
+++ vue-project/src/components/Footerbar.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: "Footerbar"
+}
+</script>
+
+<template>
+    <p>Footer</p>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-project/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: "Navbar"
+}
+</script>
+
+<template>
+    <div id="nav">
+        <RouterLink to="/">
+            <img src="" id="logo">
+        </RouterLink>
+        <RouterLink to="/">Home</RouterLink> |
+        <RouterLink to="/pedidos">Pedidos</RouterLink>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
