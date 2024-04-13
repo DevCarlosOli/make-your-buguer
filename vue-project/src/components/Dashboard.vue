6d4ebc7a95308577fe63676bc91623ebb9558591
DevCarlosOli <c.oliveiradev@gmail.com>
2024-04-13 10:10:18 -0300

    Adicionando Dashboard tela pedidos e estilizando Dashboard

---
+++ vue-project/src/components/Dashboard.vue
@@ -0,0 +1,99 @@
+<script>
+export default {
+    name: "Dashboard"
+}
+</script>
+
+<template>
+    <div id="burguer-table">
+        <div>
+            <div id="burguer-table-heading">
+                <div class="order-id">#:</div>
+                <div>Cliente:</div>
+                <div>Pão:</div>
+                <div>Carne:</div>
+                <div>Opcionais:</div>
+                <div>Ações:</div>
+            </div>
+            <div id="burguer-table-rows">
+                <div class="burguer-table-row">
+                    <div class="order-number">1</div>
+                    <div>João</div>
+                    <div>Pão de Trigo</div>
+                    <div>Maminha</div>
+                    <div>
+                        <ul>
+                            <li>Salame</li>
+                            <li>Tomate</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <select name="status" class="status">
+                            <option value="">Selecione</option>
+                        </select>
+                        <button class="deletar-btn">Cancelar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    #burguer-table {
+        max-width: 1200px;
+        margin: 0 auto;
+    }
+
+    #burguer-table-heading,
+    #burguer-table-rows,
+    .burguer-table-row {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    #burguer-table-heading {
+        font-weight: bold;
+        padding: 12px;
+        border-bottom: 3px solid #333;
+    }
+
+    #burguer-table-heading div,
+    .burguer-table-row div {
+        width: 16%;
+    }
+
+    .burguer-table-row {
+        width: 100%;
+        padding: 12px;
+        border-bottom: 1px solid #CCC;
+    }
+
+    #burguer-table-heading .order-id,
+    #burguer-table-row .order-number {
+        width: 16%;
+    }
+
+    select {
+        padding: 12px 6px;
+        margin-right: 12px;
+    }
+
+    .deletar-btn{
+        background-color: #222;
+        color: #FCBA03;
+        font-weight: bold;
+        border: 2px solid #222;
+        padding: 10px;
+        font-size: 16px;
+        margin: 0 auto;
+        cursor: pointer;
+        transition: 0.5s;
+        border-radius: 5px;
+    }
+
+    .deletar-btn:hover{
+        background-color: transparent;
+        color: #222;
+    }
+</style>
