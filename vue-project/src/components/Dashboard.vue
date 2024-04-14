d3cab55cad8bf3cac084750a807b673749ffd27f
DevCarlosOli <c.oliveiradev@gmail.com>
2024-04-13 21:50:05 -0300

    Realizando implementacao de update dos pedidos

--- vue-project/src/components/Dashboard.vue
+++ vue-project/src/components/Dashboard.vue
@@ -29,6 +29,19 @@ export default {
             const res = req.json();
 
             this.getPedidos();
+        },
+        async updateBurguer(event, id) {
+            const option = event.target.value;
+
+            const dataJson = JSON.stringify({ status: option });
+
+            const req = await fetch(`http://localhost:3000/burguers/${id}`, {
+                method: "PATCH",
+                headers: { "Content-Type": "application/json" },
+                body: dataJson
+            });
+
+            const res = await req.json();
         }
     },
     mounted() {
@@ -60,9 +73,9 @@ export default {
                         </ul>
                     </div>
                     <div>
-                        <select name="status" class="status">
+                        <select name="status" class="status" @change="updateBurguer($event, burguer.id)">
                             <option value="">Selecione</option>
-                            <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burguer.status == s.tipo">{{ s.tipo }}</option>
+                            <option v-for="s in status" :key="s.id" :value="s.tipo" :selected="burguer.status == s.tipo">{{ s.tipo }}</option>
                         </select>
                         <button class="deletar-btn" @click="deleteBurguer(burguer.id)">Cancelar</button>
                     </div>
